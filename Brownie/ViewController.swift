68e31ed08851760dc10ebf0b58d0b528497b1c26
Victor Santos <victorsantos@xds.com.br>
2021-09-21 14:45:03 -0300

    app is working printing in console the inputs

--- Brownie/ViewController.swift
+++ Brownie/ViewController.swift
@@ -9,8 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var nameFoodField: UITextField!
+    @IBOutlet var happinessField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func buttonAddTap() {
+        guard let nameFood = nameFoodField.text else { return }
+        guard let happiness = happinessField.text else { return }
+        
+        print("Comi \(nameFood) e fiquei com felicidade: \(happiness)")
     }
 }
 
